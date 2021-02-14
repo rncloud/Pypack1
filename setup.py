9f20fc3169a776e90f54521ea88775cbdee662b1
aaren-m20 <aaren-m20@users.noreply.github.com>
2021-02-14 22:08:50 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pypack',
+    version='2021.1',
+    packages=[''],
+    url='',
+    license='',
+    author='rajiv',
+    author_email='rajivnatarajan@outlook.com',
+    description='Sample'
+)
---
+++ venv/Firstpack/test_calc.py
@@ -0,0 +1,7 @@
+import pytest
+import cmd_exec
+
+def test_Add_exec():
+    N1 = 1
+    N2 = 2
+    assert Add_exec == 4
